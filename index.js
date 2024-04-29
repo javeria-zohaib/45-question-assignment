050ebfd2af7a7ac085e360b608b27d0b7fec4467
javeria-zohaib <161313643+javeria-zohaib@users.noreply.github.com>
2024-04-29 13:56:30 -0700

    assingment typescript

--- index.js
+++ index.js
@@ -365,12 +365,80 @@ if (favourite_fruits.includes("strawberries")) {
 // • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 let userName = ['Admin', 'khadija', 'Ammara', 'aiza', 'shagufta'];
-for (let i = 0; i < userName.length; i++) {
-    if (userName[i] === 'Admin') {
+// Ques:31
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+let user_Name = ['Admin', 'khadija', 'Ammara', 'aiza', 'shagufta'];
+user_Name = [];
+if (user_Name.length === 0) {
+    console.log(" We need to find some users!");
+}
+for (let i = 0; i < user_Name.length; i++) {
+    if (user_Name[i] === 'Admin') {
         console.log("Hello admin, would you like to see a status report?");
     }
     else {
-        console.log(`Hello ${userName[i]}, thank you for logging in again`);
+        console.log(`Hello ${user_Name[i]}, thank you for logging in again`);
     }
 }
 ;
+// Ques:32 
+// Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available. 
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted
+let current_users = ["humza", "areeba", "fahad", "zain", "saad"];
+let new_users = ["khizar", "ramsha", "areeba", "fahad", "zain"];
+new_users.forEach(new_one_users => {
+    let our_condition = current_users.some(current_one_users => current_one_users.toLowerCase() === new_one_users.toLowerCase());
+    if (our_condition) {
+        console.log(`${new_one_users}  is already taken`);
+    }
+    else {
+        {
+            console.log(`${new_one_users}  is  available`);
+        }
+    }
+});
+// QUES:33
+// Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let onenumber of numbers) {
+    let ordinalEnding;
+    if (onenumber === 1) {
+        ordinalEnding = "st";
+    }
+    else if (onenumber === 2) {
+        ordinalEnding = "nd";
+    }
+    else if (onenumber === 3) {
+        ordinalEnding = "rd";
+    }
+    else {
+        ordinalEnding = "th";
+    }
+    console.log(`${onenumber}${ordinalEnding}`);
+}
+// QUES:34 
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+let favourite_pizza = ["fajita", "red shawarma", "malai tikka"];
+for (let onepizza of favourite_pizza) {
+    console.log(`I like ${onepizza} pizza `);
+}
+console.log("I really love pizza");
+console.log("Pizza is not just a dish; it's a culinary masterpiece crafted with love and creativity ");
+// Ques:35
+// Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+let animal = ["Dog", "Cat", "rabbit"];
+for (let petanimal of animal) {
+    console.log(`A ${petanimal} would make a great pet`);
+}
+console.log("Any of these animals would make a great pet!");
+console.log("Pets aren't just animals; they're family members who fill our lives with unconditional love and joy");
